c06a07ed43cc5787c9ca1d4b04e6a1ad56f93f42
Borui Li <librchn@gmail.com>
2021-03-13 21:11:20 +0800

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -121,3 +121,16 @@ cd fonts
 cd ..
 rm -rf fonts
 
+## [OPTIONAL] use advanced cp and mv to show status line when copying
+wget http://ftp.gnu.org/gnu/coreutils/coreutils-8.32.tar.xz
+tar xvJf coreutils-8.32.tar.xz
+cd coreutils-8.32/
+wget https://raw.githubusercontent.com/jarun/advcpmv/master/advcpmv-0.8-8.32.patch
+patch -p1 -i advcpmv-0.8-8.32.patch
+./configure
+make
+### alias
+sudo mv ./src/cp /usr/local/bin/advcp
+sudo mv ./src/mv /usr/local/bin/advmv
+echo alias cp '/usr/local/bin/advcp -g' >> ~/.zshrc
+echo alias mv '/usr/local/bin/advmv -g' >> ~/.zshrc
